4c936f8110e13fe1dc63cf3aa028ad0111321ac8
Charles Floyd <charleswfloyd3@gmail.com>
2021-02-03 18:18:03 -0800

    main

--- src/gameover/gameover.js
+++ src/gameover/gameover.js
@@ -4,7 +4,7 @@ function GameOver(props) {
 
   return (
            <div className="game_over">
-				<p className="game_overtitle">G A M E  &nbsp; O V E R</p>
+				<p className="game_overtitle">G A M E  &nbsp; &nbsp; O V E R</p>
 		    </div> 
   )
 }
