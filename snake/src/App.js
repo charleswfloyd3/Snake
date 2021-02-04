e63168d246e1c34e4f2343d9b20c3b998850e91e
Charles Floyd <charleswfloyd3@gmail.com>
2021-02-03 17:23:58 -0800

    added more functionality

--- snake/src/App.js
+++ snake/src/App.js
@@ -11,6 +11,8 @@ function App(props) {
   const [snakeCoordinates, setsnakeCoordinates] = useState([[50,0], [50,10], [50,20]]);
   const [foodCoordinate, setFoodCoordinate] = useState("");
   const [direction, setDirection] = useState("down")
+	const [counter, setcounter] = useState(0)
+	const [gameStatus, setgameStatus] = useState(true)
 
   function isArrayInArray(arr, item){
     var item_as_string = JSON.stringify(item);
@@ -20,11 +22,44 @@ function App(props) {
     });
     return contains;
   }
+	useInterval(
+		()=>{
+		setcounter(counter+1)
+		}
+			
+		,150)
+	const eatFood = () =>{
+		if(isArrayInArray(snakeCoordinates, foodCoordinate)){
+			if(foodalive){
+				if(direction == "down"){
+					snakeCoordinates.unshift([snakeCoordinates[0][0], snakeCoordinates[0][1] - 10])
+				}
+				else if(direction == "up"){
+					snakeCoordinates.unshift([snakeCoordinates[0][0], snakeCoordinates[0][1] + 10])
+				}
+				else if(direction == "left"){
+					snakeCoordinates.unshift([snakeCoordinates[0][0] +10, snakeCoordinates[0][1]])
+				}
+				else if(direction == "right"){
+					snakeCoordinates.unshift([snakeCoordinates[0][0] -10, snakeCoordinates[0][1]])
 
+				}			
+				setfoodlifestatus(false)
+					
+			}
+			else{
+			setfoodlifestatus(true)
+
+			}
+		}
+	}		
+	useEffect(()=>{
+				moveSnake()
+				eatFood()
+
+	},[counter])
   useEffect(
         () => {
-
-    
         let randomCoordinateOptions = [0,10,20,30,40,50,60,70,80,90]
 
         let x = randomCoordinateOptions[Math.floor(Math.random() * randomCoordinateOptions.length)];
@@ -45,7 +80,7 @@ function App(props) {
 
     
   }, [foodalive])
-    
+
   const checkKey = (e) => {
     e = e || window.event;
     if (e.keyCode == '38') {
@@ -67,11 +102,11 @@ function App(props) {
   }
 
   document.onkeydown = checkKey;
-        
+         let snakeCoordinatesLive = snakeCoordinates
+			let head = snakeCoordinatesLive[snakeCoordinatesLive.length-1]     
   const moveSnake = () =>{
-    console.log("its rendering")
-      let snakeCoordinatesLive = snakeCoordinates
-      let head = snakeCoordinatesLive[snakeCoordinatesLive.length-1] 
+
+			 
       if (direction == "up") {
           head = [head[0], head[1] - 10]
           snakeCoordinatesLive.push(head)
@@ -82,7 +117,6 @@ function App(props) {
           head = [head[0], head[1] + 10]
           snakeCoordinatesLive.push(head)
           snakeCoordinatesLive.shift()
-          console.log(snakeCoordinates)          
           setsnakeCoordinates(snakeCoordinatesLive)          
       }
       else if (direction == "left") {
@@ -98,18 +132,18 @@ function App(props) {
           setsnakeCoordinates(snakeCoordinatesLive)
       }        
   }
+	// const boundaries = () =>{
+	// 	if(head[0] >= 105 || head[0] < -15 || head[1] >= 105 || head[1] < -15){
+	// 					setfoodlifestatus("eaten")
 
+	// 	}
+	// }
 
-    setTimeout(move)
-
-
-
-
-
-
+	// boundaries()
   return (
     <div className="snake_container">
-      <div className="game_grid">
+			{/* className={gameStatus ? "game_grid" : "game_over"} */}
+      <div className="game_grid">					
         <Snake coord={snakeCoordinates}/>
         <SnakeFood coord={foodCoordinate}/>
       </div>
